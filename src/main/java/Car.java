1cfe3420ea21782916856873d5dc392bfbec4491
WolfhoundAD <Loners14@yandex.ru>
2024-03-20 20:05:50 +0300

    Add TreeSet example

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,6 +1,6 @@
 import java.util.Objects;
 
-public class Car {
+public class Car implements Comparable<Car> {
     private String brand;
     private int number;
 
@@ -33,5 +33,18 @@ public class Car {
     @Override
     public int hashCode() {
         return Objects.hash(brand, number);
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return brand.compareTo(o.brand);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", number=" + number +
+                '}';
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car("BMW", 1);
-        Car car2 = new Car("BMW", 1);
-        System.out.println(car1.equals(car2));
-        System.out.println(car1.hashCode());
-        System.out.println(car2.hashCode());
+        Set<Integer> numbers = new TreeSet<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return -o1.compareTo(o2);
+            }
+        });
+        for (int i = 0; i < 100; i++) {
+            numbers.add((int)(Math.random() * 10));
+        }
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 }
